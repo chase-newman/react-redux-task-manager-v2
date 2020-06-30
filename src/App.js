1acee4292bb2c02dc17ca17d5e1743a0389eb86e
EC2 Default User <ec2-user@ip-172-31-44-187.ec2.internal>
2020-06-30 20:09:00 +0000

    create header component with working routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
+import Aux from './Components/hoc/Aux';
 import Login from './Components/Login/Login';
 import TaskManager from './Components/TaskManager/TaskManager';
 import TaskList from './Components/TaskList/TaskList';
 import { Route } from 'react-router-dom';
+import Header from './Components/Header/Header';
 import './App.css';
 
 class App extends Component {
@@ -10,13 +12,16 @@ class App extends Component {
   
   render() {
     return (
-      <div className="container">
-        <Route path="/" exact component={Login} />
-        <Route path="/task-manager" >
-          <TaskManager />
-          <TaskList />
-        </Route>
-      </div>
+      <Aux>
+        <Header />
+        <div className="container">
+          <Route path="/login" component={Login} />
+          <Route path="/task-manager" >
+            <TaskManager />
+            <TaskList />
+          </Route>
+        </div>
+      </Aux>
     );
   }
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classes from './Header.module.css';
+
+const header = () => {
+    
+    let navbarBrand = `navbar-brand ${classes.Header}`;
+    
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <span className={navbarBrand} to="#" disabled>Task Manager</span>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-item nav-link active" to="/login">Login <span class="sr-only">(current)</span></Link>
+              <Link className="nav-item nav-link" to="/task-manager">Task List</Link>
+            </div>
+          </div>
+        </nav>
+    );
+    
+    
+}
+
+
+export default header;
